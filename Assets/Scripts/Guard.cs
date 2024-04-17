d3e7f6a9a0f490a24bb75020b9d0539d764a5536
Heisenberg <66485604+NR-NJN@users.noreply.github.com>
2024-04-17 22:17:34 +0530

    guard now rotates properly

--- Assets/Scripts/Guard.cs
+++ Assets/Scripts/Guard.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class Guard : MonoBehaviour
 {
     public Transform path;
-    float speed = 5f;
-    float WaitTime = .3f;
+    public float speed = 5f;
+    public float WaitTime = .3f;
     float TurnSpeed = 90;
     private void Start()
     {
@@ -46,7 +46,7 @@ public class Guard : MonoBehaviour
         Vector3 dirToTarget = (lookTarget - transform.position).normalized;
         float targetAngle = 90 - Mathf.Atan2(dirToTarget.z, dirToTarget.x)*Mathf.Rad2Deg;
 
-        while(Mathf.Approximately(transform.eulerAngles.y, targetAngle))
+        while(Mathf.Abs(Mathf.DeltaAngle(transform.eulerAngles.y, targetAngle))>0.05)
         {
             float angle = Mathf.MoveTowardsAngle(transform.eulerAngles.y, targetAngle, TurnSpeed * Time.deltaTime);
             transform.eulerAngles = Vector3.up * angle;
