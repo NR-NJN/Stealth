8d8fb64455391f7f69f61a277e3f16015f4cdb40
Heisenberg <66485604+NR-NJN@users.noreply.github.com>
2024-04-21 21:40:12 +0530

    Updated Player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed = 5;
-   
+    public float smoothMoveTime = .1f;
+
+    float smoothInpMag;
+    float smoothMoveVelocity;
     void Update()
     {
         Vector3 inputDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        float inputMag=inputDir.magnitude;
+        smoothInpMag = Mathf.SmoothDamp(smoothInpMag, inputMag, ref smoothMoveVelocity, smoothMoveTime);
         float lookAngle = Mathf.Atan2(inputDir.x, inputDir.z)*Mathf.Rad2Deg;
         transform.eulerAngles=Vector3.up*lookAngle;
+
+        transform.Translate(translation: transform.forward* speed * Time.deltaTime*smoothInpMag, relativeTo: Space.World);
     }
 }
