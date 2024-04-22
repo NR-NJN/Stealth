6344458791d01c7d861453e4f270d9c697172624
Heisenberg <66485604+NR-NJN@users.noreply.github.com>
2024-04-22 13:55:52 +0530

    Smoother movements and turnsSpeeds

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
 {
     public float speed = 5;
     public float smoothMoveTime = .1f;
+
+    float turnSpeed = 8f;
+    float angle;
 
     float smoothInpMag;
     float smoothMoveVelocity;
@@ -15,8 +19,12 @@ public class Player : MonoBehaviour
         float inputMag=inputDir.magnitude;
         smoothInpMag = Mathf.SmoothDamp(smoothInpMag, inputMag, ref smoothMoveVelocity, smoothMoveTime);
         float lookAngle = Mathf.Atan2(inputDir.x, inputDir.z)*Mathf.Rad2Deg;
-        transform.eulerAngles=Vector3.up*lookAngle;
+
+        angle = Mathf.LerpAngle(angle, lookAngle, Time.deltaTime*turnSpeed*inputMag);
+        transform.eulerAngles=Vector3.up*angle;
 
         transform.Translate(translation: transform.forward* speed * Time.deltaTime*smoothInpMag, relativeTo: Space.World);
+
+
     }
 }
