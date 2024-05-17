bd7eef28a021c09ff31ec63483ec619995b68253
Heisenberg <66485604+NR-NJN@users.noreply.github.com>
2024-05-17 22:47:16 +0530

    had to push, because there's a separate push for octaves

--- Assets/Scripts/PCG_Terrain/Noise.cs
+++ Assets/Scripts/PCG_Terrain/Noise.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public static class Noise
 {
-    public static float[,] NoiseGenerator(int mapWidth, int mapHeight, float scale)
+    public static float[,] NoiseGenerator(int mapWidth, int mapHeight, float scale, float lacunarity, float persistance, int octaves)
     {
         float[,] noiseMap = new float[mapWidth, mapHeight];
 
@@ -12,15 +12,27 @@ public static class Noise
         {
             scale = 0.0001f;
         }
+
+        float maxNoiseHeight = float.MinValue;
+        float minNoiseHeight = float.MaxValue;
+
         for (int y = 0; y < mapHeight; y++)
         {
             for (int x = 0;x<mapWidth; x++)
             {
-                float sampleW= x/scale;
-                float sampleH= y/scale;
 
-                float perlinValue= Mathf.PerlinNoise(sampleW, sampleH);
-                noiseMap[x,y]= perlinValue;
+                float amplitude = 1;
+                float frequency = 1;
+                float noiseHeight = 0;
+                for (int i = 0; i < octaves; i++)
+                {
+                    float sampleW = x / scale*frequency;
+                    float sampleH = y / scale*frequency;
+
+                    float perlinValue = Mathf.PerlinNoise(sampleW, sampleH) * 2 - 1; // Value between -1.0 and 1.0.
+                    noiseHeight += perlinValue*amplitude;
+                }
+                noiseMap[y, x] = noiseHeight;
             }
         }
 
